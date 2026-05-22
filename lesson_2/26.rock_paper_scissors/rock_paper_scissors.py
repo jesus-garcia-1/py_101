31cf37ff47519bffb5f2deba8601fce8c1982a22
Jesus Garcia <jesusgarcimodi@gmail.com>
2026-05-22 16:26:10 +0200

    modifications rock_paper_scissors

--- lesson_2/26.rock_paper_scissors/rock_paper_scissors.py
+++ lesson_2/26.rock_paper_scissors/rock_paper_scissors.py
@@ -1,21 +1,6 @@
 import random
-
-
-# rock wins scsissors
-# rock wins lizard
-# lizard wins paper
-# lizard wins spock
-# paper wins rock
-# paper wins spock
-# scissors wins paper
-# scissor wins lizard
-# spock wins rock
-# spock wins scissors
-
-# We want to add points to the computer_counter and user_counter, however
-# they are global variables ??
-
-# I want  a function that checks if winner is computer or the user
+import os
+import time
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 counter = {'user': 0 , 'computer': 0}
@@ -32,15 +17,15 @@ def prompt(message):
     print(f"==> {message}")
 
 def get_choice(user_string):
-    if user_string.lower().startswith('r'):
+    if user_string.lower().strip().startswith('r'):
         return 'rock'
-    if user_string.lower().startswith('sc'):
+    if user_string.lower().strip().startswith('sc'):
         return 'scissors'
-    if user_string.lower().startswith('sp'):
+    if user_string.lower().strip().startswith('sp'):
         return 'spock'
-    if user_string.lower().startswith('l'):
+    if user_string.lower().strip().startswith('l'):
         return 'lizard'
-    if user_string.lower().startswith('p'):
+    if user_string.lower().strip().startswith('p'):
         return 'paper'
     return False
 
@@ -48,19 +33,10 @@ def get_choice(user_string):
 def player_wins(player_choice, computer):
     return computer in WINING_COMBOS[player_choice]
 
-def tie(player_choice, computer):
-    return player_choice == computer
-
-def add_points(player, computer):
-    if player_wins(player, computer):
-        counter['user'] += 1
-    elif not tie(player, computer):
-        counter['computer'] +=1
-
 def display_winner(play_choice, computer):
     if player_wins(play_choice, computer):
         prompt('You win!')
-    elif tie(play_choice, computer):
+    elif play_choice == computer:
         prompt("It's a tie!")
     else:
         prompt('Computer wins!')
@@ -77,37 +53,59 @@ def display_max_winner(point):
         prompt(f'''Computer is the max winner, you obtained {point['user']}
         points and the computer obtained {point['computer']} points''')
 
+def clear_screen():
+    os.system('clear')
+
+def get_validated_choice(user_answer):
+
+    while user_answer not in VALID_CHOICES :
+        prompt("That's not a valid choice, please try again")
+        user_choicee = input()
+        user_answer = get_choice(user_choicee)
+
+    return user_answer
+
+clear_screen()
+prompt('Welcome  back to Rock, Paper, Scissors, Lizard, Spock')
 while True:
+    answer = 'yes'
     prompt(f'''Choose one: {', '.join(VALID_CHOICES)},
     you can write just the first letters''')
 
     user_choice = input()
     choice = get_choice(user_choice)
 
-    while choice not in VALID_CHOICES :
-        prompt("That's not a valid choice, please try again")
-        user_choice = input()
-        choice = get_choice(user_choice)
+    choice = get_validated_choice(choice)
 
     computer_choice = random.choice(VALID_CHOICES)
 
     prompt(f'You chose {choice}, the computer chose {computer_choice}')
-
     display_winner(choice, computer_choice)
-    add_points(choice, computer_choice)
 
-    while True:
-        prompt('Do you want to play another game? ( y/n )')
-        answer = input().lower()
+    if player_wins(choice, computer_choice):
+        counter['user'] += 1
+    elif choice != computer_choice:
+        counter['computer'] +=1
 
-        if answer.startswith('y') or answer.startswith('n'):
-            break
-
-        prompt("That's not a valid answer")
-
-    if answer[0] == 'n':
-        break
+    prompt(f'''Current score - You: {counter['user']},
+    Computer: {counter['computer']} ''')
+    time.sleep(2)
+    clear_screen()
 
     if game_ends(counter):
         display_max_winner(counter)
+        prompt('Do you want to play another game? ( y/n )')
+        answer = input().lower().strip()
+
+        while True:
+            if answer.startswith('y') or  answer.startswith('n'):
+                break
+
+            prompt("That's not a valid answer")
+            prompt('Do you want to play another game? ( y/n )')
+            answer = input().lower().strip()
+
+        counter = {'user': 0 , 'computer': 0}
+
+    if answer[0] == 'n':
         break
